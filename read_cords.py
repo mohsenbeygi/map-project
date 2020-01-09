b33d528d52d7a75444cb8649c7bc6e9a95a6fb66
mohsenbeygi <53968970+mohsenbeygi@users.noreply.github.com>
2020-01-09 19:04:41 +0100

    cleaning and compatibility fixes

---
+++ read_cords.py
@@ -0,0 +1,98 @@
+import os
+
+
+def get_download_path():
+    """
+    returns the default downloads
+    path for any os (linux or windows or mac)
+    """
+    if os.name == 'nt':
+        return os.path.join(os.path.expanduser('~'),
+                            'Downloads')
+    else:
+        return os.path.join(os.path.expanduser('~'),
+                            'downloads')
+
+
+def get_cords(filename):
+    '''
+    read file extract first
+    cord (longitude or latitude)
+    '''
+
+    path = os.path.exists(os.path.join(get_download_path(),
+                                       filename))
+    print(path)
+    if not os.path.exists(os.path.join(get_download_path(),
+                                       filename)):
+        print(filename + " doesn't exist")
+        return
+
+    delete = False
+
+    with open(os.path.join(get_download_path(),
+                           filename), 'r') as file:
+        data = file.readline()
+
+        if len(data) < 2:
+            print(filename + " is empty (no cordinations)")
+
+            # empty so remove it
+            delete = True
+
+        else:
+            index = data.find(',')
+            cord = float(data[:index])
+
+    if delete:
+        delete_file(filename)
+        return
+
+    with open(os.path.join(get_download_path(),
+                           filename), 'w') as file:
+        file.write(data[index + 1:])
+
+    return cord
+
+
+def delete_file(filename):
+    # empty so remove it
+    os.remove(os.path.join(get_download_path(),
+                           filename))
+    print("deleted " + filename)
+    return
+
+
+def valid_data(node1, node2, filename):
+    if node1[0] is None or node1[1] is None:
+        raise ValueError(" cordination data in file " +
+                         filename + ", wasn't valid !")
+
+    if node2[0] is None or node2[1] is None:
+        raise ValueError(" cordination data in file " +
+                         filename + ", wasn't valid !")
+
+
+def get_node_cords(filename):
+    # read from file
+    lat1 = get_cords(filename)
+    lon1 = get_cords(filename)
+    node1 = (lat1, lon1)
+    print("node1: ", node1)
+
+    lat2 = get_cords(filename)
+    lon2 = get_cords(filename)
+    node2 = (lat2, lon2)
+    print("node2: ", node2)
+
+    valid_data(node1, node2, filename)
+
+    # delete file
+    get_cords(filename)
+
+    return node1, node2
+
+
+if __name__ == "__main__":
+    filename = "cords.txt"
+    print(get_cords(filename))
---
+++ router.py
@@ -0,0 +1,117 @@
+from load.map import Map
+from path.dijkstra2 import *
+import mplleaflet
+import matplotlib.pyplot as plt
+from read_cords import *
+
+
+# infinity for routing
+inf = float('inf')
+
+
+class Router:
+    def __init__(self, transport):
+        self.transport = transport
+        # self.map = Map("car")
+        self.map = Map("car", read=True)
+        self.map.read("./maps/map_graph.json")
+
+    def display_path(self, path, node1, node2):
+        # showing on map with mplleaflet library
+        lons = []
+        lats = []
+        for i in path:
+            node = self.map.node_cords[i]
+            lats.append(node[0])
+            lons.append(node[1])
+        fig = plt.figure()
+        plt.plot(lons, lats, color="purple",
+                 linewidth=10)
+        # draw red square for start of path
+        plt.plot([self.map.node_cords[node1][1]],
+                 [self.map.node_cords[node1][0]],
+                 "rs")
+        # draw red square for end of path
+        plt.plot([self.map.node_cords[node2][1]],
+                 [self.map.node_cords[node2][0]],
+                 "rs")
+        mplleaflet.show(fig=fig)
+
+    def get_costs(self, start):
+        # create costs data structure
+        costs = {}
+        for node in self.map.node_cords:
+            if node == start:
+                costs[node] = 0
+            else:
+                costs[node] = inf
+        return costs
+
+    def find_path(self, node1, node2):
+        # find the closests node to the
+        # given latitude and longitude
+        self.map.get_distances(node2)
+        node1 = self.map.find_node(*node1)
+        node2 = self.map.find_node(*node2)
+
+        if node1 == node2:
+            raise ValueError("start and \
+                destination are equal !")
+            return
+
+        # print(self.map.node_cords[node1])
+        # print(self.map.node_cords[node2])
+
+        ''' dijkstra routing '''
+        # # create costs data structure
+        # costs = self.get_costs(node1)
+        # # print(self.map.graph['2170382002'])
+        # parents = dijkstra(costs,
+        #                    self.map.graph,
+        #                    node2,
+        #                    self.map.distances)
+
+        ''' dijkstra2 routing '''
+        parents = dijkstra(self.map.graph,
+                           node1,
+                           self.map.distances,
+                           sink=node2
+                           )
+
+        # get path from node1 to node2
+        path = get_path(parents, node1, node2)[::-1]
+        # print('Shortest path from ', node1,
+        #       ' to ', node2, ' is: ', path)
+
+        self.display_path(path, node1, node2)
+
+
+if __name__ == '__main__':
+
+    # enter latitude and longitude for
+    # the start and destination positions
+    read_cordinations = True
+
+    if not read_cordinations:
+        # start node
+        node1 = (35.80367469044131, 51.438760757446296)
+        # destination node
+        node2 = (35.801312188809185, 51.43841207027436)
+
+        '''
+        get start and end from user
+        '''
+
+        # node1 = tuple(map(float,
+        #     input("Beginning:  ").split(',')))
+        # node2 = tuple(map(float,
+        #     input("Destination:  ").split(',')))
+
+    else:
+        node1, node2 = get_node_cords("cords.txt")
+
+    # create router object with specific transport type
+    router = Router("car")
+
+    # find path and display path on map
+    router.find_path(node1, node2)
